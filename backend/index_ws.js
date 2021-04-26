7a67b8c34a71bda5f9c4db9805a0e53451a36624
Abobakr <abobakryousre@gmail.com>
2021-04-26 02:59:57 +0200

    websocet send message t spicific user

--- backend/index_ws.js
+++ backend/index_ws.js
@@ -2,29 +2,21 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const server = http.createServer(app);
-const io = require("socket.io")(server, { cors: { origin: "*" } });
-const clients = [];
+const io = require("socket.io")(server, {
+  cors: { origin: "*", methods: "*" },
+});
+const ids = [];
 io.on("connection", (socket) => {
-  console.log("socket Connected ", socket.id);
-  socket.on("message", (data) => {
-    console.log("boradcast ", data);
-    socket.broadcast.emit("boradCast", data);
+  console.log("Connected id ", socket.id);
+  ids.push(socket.id);
+  socket.emit("updateIds", ids);
+  socket.broadcast.emit("updateIds", ids);
+  socket.on("sendToTarget", (data) => {
+    console.log("sendToTarget the following  ", data);
+    socket.to(data.targetID).emit("recevieMessage", data.message);
   });
 });
 
-// io.on("connection", (socket) => {
-//   console.log("socket Connected ", socket.id);
-//   // adding new connedted client
-//   clients.push(socket.id);
-
-//   socket.on("message", (socketId, message) => {
-//     socket.to(socketId).emit("new-messsage", message);
-//   });
-// });
-
-app.get("/clients", (req, res) => {
-  res.json(clients);
-});
 server.listen(3001, (err) => {
   if (err) return console.log(err);
   console.log("Server Running on port 3001");
